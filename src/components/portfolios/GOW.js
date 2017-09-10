3fa4863ddd0c54150c2c42b5b6f082b3c6bd4451
Anh Mai <anhmaidesign@gmail.com>
2017-09-10 13:50:17 -0700

    Updated

--- src/components/portfolios/GOW.js
+++ src/components/portfolios/GOW.js
@@ -123,6 +123,7 @@ class GOW extends React.Component {
             </div>
           </div>
         </div>
+      {/*
         <div className="row">
           <div className="col-xs-12 portfolio-download">
             <FlatButton
@@ -135,95 +136,50 @@ class GOW extends React.Component {
             />
           </div>
         </div>
+        */}
         <div className="row">
           <div className="col-xs-12 col-sm-9 col-sm-offset-3">
-            <div className="portfolio-group">Project Narrative</div>
-            <div className="portfolio-subgroup">Client & Product</div>
+            <div className="portfolio-subtitle">Project Narrative</div>
           </div>
         </div>
         <div className="row portfolio-row">
           <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
             <div className="portfolio-left-col">
-              Client
+              Client & Product
             </div>
           </div>
           <div className="col-xs-12 col-sm-9">
             <div className="portfolio-right-col">
+              <p><b>My Client</b></p>
               <p>
-              Garden Of Wisdom ( GOW) is a family business based in Prescott, Arizona. Their goal is to formulate and stock unique and effective skin care products to better assist customers and their unique skin types in finding a happy while sustaining a high level of skin health.
+              Garden Of Wisdom (GOW) is a family business based in Prescott, Arizona. Their goal is to formulate and stock unique and effective skin care products to better assist customers and their unique skin types in finding a happy while sustaining a high level of skin health.
               </p>
-            </div>
-          </div>
-        </div>
-        <div className="row portfolio-row">
-          <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
-            <div className="portfolio-left-col">
-              Target Audiences
-            </div>
-          </div>
-          <div className="col-xs-12 col-sm-9">
-            <div className="portfolio-right-col">
+              <p><b>Target Audiences</b></p>
               <p>
               The main target audience is women and perhaps men between the ages of 20 to 50 who are looking forward to having a healthy skin with affordable prices.
               </p>
-            </div>
-          </div>
-        </div>
-        <div className="row portfolio-row">
-          <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
-            <div className="portfolio-left-col">
-              My Task & Goal
-            </div>
-          </div>
-          <div className="col-xs-12 col-sm-9">
-            <div className="portfolio-right-col">
+              <p><b>My Task & Goal</b></p>
               <p>
               Understanding that GOW is just a family business, they do not have a big budget so the current website design confuses their customers and it does not look like they sell good quality products. In addition, if a customer wants to buy skin care to treat themselves, GOW is not a good choice for them because the user interface makes them feel uncomfortable and not welcoming. That is why this website needs to be fixed to gain more customers.
               </p>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-xs-12 col-sm-9 col-sm-offset-3">
-            <div className="portfolio-subgroup">Creative Brief</div>
-          </div>
-        </div>
         <div className="row portfolio-row">
           <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
             <div className="portfolio-left-col">
-              Current Problems
+              Creative Brief
             </div>
           </div>
           <div className="col-xs-12 col-sm-9">
             <div className="portfolio-right-col">
+              <p><b>Current Problems</b></p>
               <p>
               The current GOW site has too many unnecessary features and lack of a clear message. Poor navigation bar confuses customers and slows down the shopping experience.
               </p>
-            </div>
-          </div>
-        </div>
-        <div className="row portfolio-row">
-          <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
-            <div className="portfolio-left-col">
-              Solutions
-            </div>
-          </div>
-          <div className="col-xs-12 col-sm-9">
-            <div className="portfolio-right-col">
-              <p>
-              My site content will be based on the current Garden Of Wisdom (GWO) website. I will focus on the navigation bar and shopping features that will bring to customers more special information as well as guide customer to the page they want.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="row portfolio-row">
-          <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
-            <div className="portfolio-left-col">
-              Measure Success
-            </div>
-          </div>
-          <div className="col-xs-12 col-sm-9">
-            <div className="portfolio-right-col">
+              <p><b>Solutions</b></p>
+              <p>My site content will be based on the current Garden Of Wisdom (GWO) website. I will focus on the navigation bar and shopping features that will bring to customers more special information as well as guide customer to the page they want.</p>
+              <p><b>Measure Success</b></p>
               <p>
               The success of this site will be measured based on following criteria:
               </p>
@@ -239,15 +195,10 @@ class GOW extends React.Component {
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-xs-12 col-sm-9 col-sm-offset-3">
-            <div className="portfolio-subgroup">Competitive Analysis</div>
-          </div>
-        </div>
         <div className="row portfolio-row">
           <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
             <div className="portfolio-left-col">
-              Competitors
+              Competitive Analysis
             </div>
           </div>
           <div className="col-xs-12 col-sm-9">
