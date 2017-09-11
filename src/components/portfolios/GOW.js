37451c09e7e1f29c005baed6869958b8f047a152
Anh Mai <anhmaidesign@gmail.com>
2017-09-10 18:32:03 -0700

    Updated

--- src/components/portfolios/GOW.js
+++ src/components/portfolios/GOW.js
@@ -6,6 +6,15 @@ import Back from 'material-ui/svg-icons/navigation/arrow-back';
 import Forward from 'material-ui/svg-icons/navigation/arrow-forward';
 import LinkIcon from 'material-ui/svg-icons/content/link';
 import FlatButton from 'material-ui/FlatButton';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+
 
 const styles = {
   mediumIcon: {
@@ -203,7 +212,185 @@ class GOW extends React.Component {
           </div>
           <div className="col-xs-12 col-sm-9">
             <div className="portfolio-right-col">
-              <img className="portfolio-img-page" src="files/gow-competitive-analysis.png"/>
+              <Table style={{background: 'transparent'}}>
+                <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                  <TableRow>
+                    <TableHeaderColumn><b>FACTOR</b></TableHeaderColumn>
+                    <TableHeaderColumn><b>GARDEN OF WISDOM</b></TableHeaderColumn>
+                    <TableHeaderColumn><b>SEPHORA</b></TableHeaderColumn>
+                    <TableHeaderColumn><b>KIARA PHITOCEUTICALS</b></TableHeaderColumn>
+                  </TableRow>
+                </TableHeader>
+                <TableBody displayRowCheckbox={false}>
+                  <TableRow striped={false}>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}><b>Product</b></TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Skincare, especially oils</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Everything about beauty products</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Skincare, especially facial oils</TableRowColumn>
+                  </TableRow>
+                  <TableRow striped={false}>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}><b>Price</b></TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>$5 - $35 depending on the size</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>$20 and up</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>$25 and up</TableRowColumn>
+                  </TableRow>
+                  <TableRow striped={false}>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}><b>Quality</b></TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>100% pure essential oils</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>High quality products</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>High quality products</TableRowColumn>
+                  </TableRow>
+                  <TableRow striped={false}>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}><b>Strength</b></TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Familly business so they mainly focus on quality with affordable prices</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Big company so they have more brand options with the best customer services</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Top oil brand</TableRowColumn>
+                  </TableRow>
+                  <TableRow striped={false}>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}><b>Weakness</b></TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>No stores, online shopping only with a few choice of products</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>High prices with many brands lead to difficulty selection</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Only available in Australia</TableRowColumn>
+                  </TableRow>
+                  <TableRow striped={false}>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}><b>Apperance</b></TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Simple packaging with no brand identity</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Colorful looking</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Only black and white</TableRowColumn>
+                  </TableRow>
+                  <TableRow striped={false}>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}><b>Advertising</b></TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>No marketing plan</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Social media advertsing with email subscribes</TableRowColumn>
+                    <TableRowColumn style={{whiteSpace: 'initial'}}>Email subscribes</TableRowColumn>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </div>
+          </div>
+        </div>
+        <div className="row portfolio-row">
+          <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
+            <div className="portfolio-left-col">
+              User Persona
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-9">
+            <div className="portfolio-right-col">
+              <div className="col-xs-12 col-sm-4">
+                <img className="portfolio-img-page" style={ {marginTop: '20px'}} src="files/people/sarah.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-8">
+                <h3>Sarah Greet</h3>
+                <p>30, Female</p>
+                <p>Teacher</p>
+                <p>Britain</p>
+                <p>Sarah is an English teacher who always stays in the office because of the nature of her work. She lives with her cat named Lila and in her free time, she usually spends the time to travel around the world to explore different cultures and cuisines.</p>
+                <p>She has been shopping several different skin care brands to find the best skin care routine for her dry skin. She loves GOW products because of its quality and prices. Also, she does not care much about product ingredients, but she would like to have a better shopping experience at GOW website.</p>
+                <p><b>Current Frustrations:</b></p>
+                <ul>
+                  <li>The navigation bar is difficult to use when shopping</li>
+                  <li>Overwhelming information on the website</li>
+                </ul>
+                <p><b>User Needs:</b></p>
+                <ul>
+                  <li>Navigation bar structure is simple and easy to navigate</li>
+                  <li>Promotion information displays more clearly</li>
+                </ul>
+              </div>
+              <div className="col-xs-12 col-sm-4">
+                <img className="portfolio-img-page" style={ {marginTop: '20px'}} src="files/people/alen.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-8">
+                <h3>Alen Smith</h3>
+                <p>23, Male</p>
+                <p>Dancer</p>
+                <p>Redmond, WA</p>
+                <p>Alen has been working as a freelance dancer since he was in secondary school. He does not like working for others, so most of the time he does what he wants. He has a youtube channel where he posts his dancing tutorials and his travel diary.</p>
+                <p>He compares himself as the free bird. He has shopped at GOW several times in the past but he stopped it because he had he has many bad experience when shopping at GOW website.</p>
+                <p><b>Current Frustrations:</b></p>
+                <ul>
+                  <li>Information is fragmented and difficult to control</li>
+                  <li>Lack of images and product description</li>
+                </ul>
+                <p><b>User Needs:</b></p>
+                <ul>
+                  <li>A nice and clean website layout helps to find product easier</li>
+                  <li>More functions to support purchase</li>
+                  <li>More featured products</li>
+                </ul>
+              </div>
+              <div className="col-xs-12 col-sm-4">
+                <img className="portfolio-img-page" style={ {marginTop: '20px'}} src="files/people/tran.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-8">
+                <h3>Tran Pham</h3>
+                <p>23, Female</p>
+                <p>Student</p>
+                <p>Vietnam</p>
+                <p>Tran is an international student who is studying at Seattle Central College. She loves cooking, hiking, dancing and traveling with her friends.</p>
+                <p>GOW is one of her favorite places to buy skin care products even though her shopping experience was not always good. She called herself as a hard person, always looking for the best effective and affordable products for her combination skin.</p>
+                <p><b>Current Frustrations:</b></p>
+                <ul>
+                  <li>Too many steps to navigate to the product page</li>
+                  <li>Lack of promotion information</li>
+                  <li>Poor navigation barand details are not streamlined</li>
+                </ul>
+                <p><b>User Needs:</b></p>
+                <ul>
+                  <li>More product images with prices</li>
+                  <li>Less crowded navigation bar</li>
+                  <li>Shipping options more stand out</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row portfolio-row">
+          <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
+            <div className="portfolio-left-col">
+              Logic Flow
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-9">
+            <div className="portfolio-right-col">
+              <p><i>The flow chart is a means to visually present the flow of data throughout the new GOW layout.</i></p>
+              <img className="portfolio-img-page" style={ {marginTop: '20px'}} src="files/gow_flow_chart.png"/>
+            </div>
+          </div>
+        </div>
+        <div className="row portfolio-row">
+          <div className="col-xs-12 col-sm-3 portfolio-vertical-align">
+            <div className="portfolio-left-col">
+              Sketches
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-9">
+            <div className="portfolio-right-col">
+              <div className="col-xs-12 col-sm-6">
+                <img className="portfolio-img-page" src="files/gow_sketches/Sketch1.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <img className="portfolio-img-page" src="files/gow_sketches/Sketch2.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <img className="portfolio-img-page" src="files/gow_sketches/Sketch3.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <img className="portfolio-img-page" src="files/gow_sketches/Sketch4.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <img className="portfolio-img-page" src="files/gow_sketches/Sketch5.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <img className="portfolio-img-page" src="files/gow_sketches/Sketch6.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <img className="portfolio-img-page" src="files/gow_sketches/Sketch7.jpg"/>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <img className="portfolio-img-page" src="files/gow_sketches/Sketch8.jpg"/>
+              </div>
             </div>
           </div>
         </div>
